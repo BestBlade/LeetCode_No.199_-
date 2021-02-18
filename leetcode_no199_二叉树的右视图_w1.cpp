e19a6c295040899ea1fe900a2497666904f5ac9b
BestBlade <68151785+BestBlade@users.noreply.github.com>
2021-02-18 17:41:32 +0800

    Add files via upload

---
+++ leetcode_no199_二叉树的右视图_w1.cpp
@@ -0,0 +1,89 @@
+﻿/* ------------------------------------------------------------------|
+给定一棵二叉树，想象自己站在它的右侧，按照从顶部到底部的顺序，返回从右侧所能看到的节点值。
+
+示例:
+
+输入: [1,2,3,null,5,null,4]
+输出: [1, 3, 4]
+解释:
+
+   1            <---
+ /   \
+2     3         <---
+ \     \
+  5     4       <---
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/binary-tree-right-side-view
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。|
+-------------------------------------------------------------------*/
+
+/*
+*
+*	执行用时：0 ms, 在所有 C++ 提交中击败了100.00%的用户
+*	内存消耗：11.6 MB, 在所有 C++ 提交中击败了67.54%的用户
+*/
+
+#include <iostream>
+#include <string> 
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+#include <map>
+#include <stack>
+#include <queue>
+
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode(int x) : val(x), next(NULL) {}
+
+};
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* next;
+
+    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val, Node* _left, Node* _right, Node* _next)
+        : val(_val), left(_left), right(_right), next(_next) {}
+};
+
+vector<int> rightSideView(TreeNode* root) {
+    if (!root) {
+        return {};
+    }
+    queue<TreeNode*> q;
+    q.emplace(root);
+    vector<int> res;
+    while (!q.empty()) {
+        int l = q.size();
+        res.push_back(q.front()->val);
+        while (l--) {
+            TreeNode* curr = q.front();
+            q.pop();
+            if (curr->right) {
+                q.emplace(curr->right);
+            }
+            if (curr->left) {
+                q.emplace(curr->left);
+            }
+        }
+    }
+    return res;
+}
